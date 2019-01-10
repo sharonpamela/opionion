97ad2b53698a8fbead54182643ae5246cb40e0da
sharonpamela <sharon.pamela.santana@gmail.com>
2019-01-10 07:48:14 -0400

    clean start

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/',(req,res) =>{
   res.send({hi:'there'});
 });
 
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT
 app.listen(PORT);
