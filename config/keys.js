7df1bd7f553707c9e1115d21d93970681d018912
sharonpamela <sharon.pamela.santana@gmail.com>
2019-06-15 20:14:12 -0700

    fixing typos

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // figures out what set of credentials are needed
 
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     // we are prod, return the prod set of keys
     module.exports = require('./prod');
 } else {
