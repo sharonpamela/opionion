0e3f6bf4a10fd5f6643cf5bc50fbc34d64a1eac6
sharonpamela <sharon.pamela.santana@gmail.com>
2019-06-16 18:27:44 -0700

    cleaned up index.js

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const keys = require('./config/keys');
-const cookieSession =  require('cookie-session');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 require('./models/User');
-require ('./services/passport');
+require('./models/Survey');
+require('./services/passport');
 
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+if (process.env.NODE_ENV === 'production') {
 
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
